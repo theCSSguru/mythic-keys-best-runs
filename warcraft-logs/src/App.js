f1c1aae01e8433c905d100b58e63ee5ce7816b55
theCSSguru <cpj238@gmail.com>
2022-04-23 21:05:23 -0400

    added class select but still need to make it work

--- warcraft-logs/src/App.js
+++ warcraft-logs/src/App.js
@@ -4,8 +4,23 @@ import './App.scss';
 function App() {
   const characterLevelMax = 60;
   const characterLevelArray = Array.from(Array(characterLevelMax).keys());
+  const characterClassArray = Array.from(Array(12).keys());
   const [characters, setCharacters] = useState([]);
   const [characterLevel, setCharacterLevel] = useState(characterLevelMax);
+  //const [characterClass, setCharacterClass] = useState(null);
+
+  // const characterLevelMax = 60;
+  // const characterLevelArray = Array.from(Array(characterLevelMax).keys());
+  // const characterClassArray = Array.from(Array(12).keys());
+  // const [characters, setCharacters] = useState([]);
+  // const [characterLevel, setCharacterLevel] = useState(characterLevelMax);
+  // const [characterClass, setCharacterClass] = useState(null);
+  // const characterFilterLevel = characters.members.filter(member => member.character.level === characterLevel);
+  // const characterFilterClass = characters.members.filter(
+  //   member => member.character.playable_class.id === characterClass
+  // );
+  // const [characterFilter, setCharacterFilter] = useState(characterFilterLevel);
+  // const characterCount = characterFilter.length;
 
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
@@ -14,7 +29,7 @@ function App() {
 
   useEffect(() => {
     fetch(
-      `https://us.api.blizzard.com/data/wow/guild/${serverName}/${guildName}/roster?namespace=profile-us&locale=en_US&access_token=USU4dJSlxXbxtsnkM6Eyyl4JRkx7rh3kXI`
+      `https://us.api.blizzard.com/data/wow/guild/${serverName}/${guildName}/roster?namespace=profile-us&locale=en_US&access_token=${process.env.REACT_APP_API_KEY}`
     )
       .then(res => res.json())
       .then(
@@ -41,15 +56,63 @@ function App() {
     setCharacterLevel(parseInt(event.target.value));
   }
 
+  // function handleCharacterClass(event) {
+  //   setCharacterClass(parseInt(event.target.value));
+  // }
+
+  // function handleCharacterLevel(event) {
+  //   setCharacterLevel(parseInt(event.target.value));
+  //   setCharacterFilter(characterFilterLevel);
+  // }
+
+  // function handleCharacterClass(event) {
+  //   setCharacterClass(parseInt(event.target.value));
+  //   setCharacterFilter(characterFilterClass);
+  // }
+
+  function wowClassNames(id) {
+    switch (id) {
+      case 1:
+        return 'Warrior';
+      case 2:
+        return 'Paladin';
+      case 3:
+        return 'Hunter';
+      case 4:
+        return 'Rogue';
+      case 5:
+        return 'Priest';
+      case 6:
+        return 'Death Knight';
+      case 7:
+        return 'Shaman';
+      case 8:
+        return 'Mage';
+      case 9:
+        return 'Warlock';
+      case 10:
+        return 'Monk';
+      case 11:
+        return 'Druid';
+      case 12:
+        return 'Demon Hunter';
+      default:
+        return null;
+    }
+  }
+
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div className='wrapper'>Error: {error.message}</div>;
   } else if (!isLoaded) {
-    return <div>Loading...</div>;
+    return <div className='wrapper'>Loading...</div>;
   } else {
     const characterLevelFilter = characters.members.filter(member => member.character.level === characterLevel);
+    // const characterClassFilter = characters.members.filter(
+    //   member => member.character.playable_class.id === characterClass
+    // );
     const characterCount = characterLevelFilter.length;
     return (
-      <div className='wrapper'>
+      <div className={`wrapper ${characters.guild.faction.name}`}>
         <form onSubmit={handleGuildSearch}>
           <input type='search' name='guild-name' id='guild-name' placeholder='Guild Name' />
           <input type='search' name='server-name' id='server-name' placeholder='Server Name' />
@@ -57,20 +120,38 @@ function App() {
             Search
           </button>
         </form>
+        <small>U.S. Servers Only</small>
         <h1>
           <span className={characters.guild.faction.name}>{`<${characters.guild.name}>`}</span>
           <em>- {characters.guild.realm.name}</em>
         </h1>
-        <label htmlFor='character-level'>Select Character Level</label>
-        <select name='character-level' id='character-level' onChange={handleCharacterLevel}>
-          {characterLevelArray
-            .sort((a, b) => (a - b ? -1 : 1))
-            .map((item, i) => (
-              <option key={i} value={item + 1}>
-                {item + 1}
+        <div className='selectors'>
+          <div className='selector'>
+            <label htmlFor='character-level'>Select Character Level</label>
+            <select name='character-level' id='character-level' onChange={handleCharacterLevel}>
+              {characterLevelArray
+                .sort((a, b) => (a - b ? -1 : 1))
+                .map((item, i) => (
+                  <option key={i} value={item + 1}>
+                    {item + 1}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className='selector'>
+            <label htmlFor='character-class'>Select Class</label>
+            {/* <select name='character-class' id='character-class' value='DEFAULT' onChange={handleCharacterClass}>
+              <option value='DEFAULT' disabled>
+                Select Class
               </option>
-            ))}
-        </select>
+              {characterClassArray.map((item, i) => (
+                <option key={i} value={item + 1}>
+                  {wowClassNames(item + 1)}
+                </option>
+              ))}
+            </select> */}
+          </div>
+        </div>
         <h2>
           <em>{characterCount}</em> Total Characters at <span>Level {characterLevel}</span>
         </h2>
@@ -79,10 +160,10 @@ function App() {
             <li key={item.character.id}>
               <em>{characterLevel}:</em>{' '}
               <a
-                href={`https://worldofwarcraft.com/en-us/character/us/firetree/${item.character.name}`}
+                href={`https://worldofwarcraft.com/en-us/character/us/${characters.guild.realm.slug}/${item.character.name}`}
                 target='_blank'
                 rel='noreferrer'
-                data-wow-class={item.character.playable_class.id}
+                data-wow-class={wowClassNames(item.character.playable_class.id)}
               >
                 {item.character.name}
               </a>
